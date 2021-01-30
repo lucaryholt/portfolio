756225958c82a6f9ea930dcf78fb826f9047ea42
Luca Ryholt <lucaryholt@gmail.com>
2021-01-30 22:50:07 +0100

    added support for modal projects

--- public/index.js
+++ public/index.js
@@ -5,7 +5,8 @@ $.ajax({
         appendPage(page, '#live-projects-holder');
     });
     response.other.map(page => {
-        appendPage(page, '#other-projects-holder');
+        if (page.type === 'link') appendPage(page, '#other-projects-holder');
+        else appendModal(page, '#other-projects-holder');
     });
 });
 
@@ -21,5 +22,37 @@ function appendPage(data, id) {
                 ` : ${data.description}` +
             `</span>` +
         `</div>`
+    );
+}
+
+function appendModal(data, id) {
+    const pageHolder = $(id);
+
+    pageHolder.append(
+        `<div class="project">
+            <span>
+                <a class="link-holder" data-toggle="modal" data-target="#exampleModal"><strong>${data.name}</strong></a>
+                 : ${data.description}
+            </span>
+        </div>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel"><strong>${data.name}</strong></h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ${data.text}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>`
     );
 }
