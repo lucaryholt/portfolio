30c1273ee62c433aa5c1457690f3dc25b0ea0f2c
Luca Ryholt <lucaryholt@gmail.com>
2021-01-31 00:36:49 +0100

    changed to collapse instead

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ $.ajax({
     });
     response.other.map(page => {
         if (page.type === 'link') appendPage(page, '#other-projects-holder');
-        else appendModal(page, '#other-projects-holder');
+        else appendCollapse(page, '#other-projects-holder');
     });
 });
 
@@ -25,34 +25,20 @@ function appendPage(data, id) {
     );
 }
 
-function appendModal(data, id) {
+function appendCollapse(data, id) {
     const pageHolder = $(id);
 
     pageHolder.append(
         `<div class="project">
             <span>
-                <a class="link-holder" data-toggle="modal" data-target="#exampleModal"><strong>${data.name}</strong></a>
+                <a class="link-holder collapse-trigger" href="#${data.name}" data-toggle="collapse" aria-expanded="false" aria-controls="skills-collapse">
+                    <strong>${data.name}</strong>
+                </a>
                  : ${data.description}
             </span>
         </div>
-
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel"><strong>${data.name}</strong></h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        ${data.text}
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
+        <div class="collapse" id="${data.name}">
+            <div class="collapse-text">${data.text}</div>
         </div>`
     );
 }
