4a4705da67ac00869254c12cc42fc551dfb8e027
Luca Ryholt <lucaryholt@gmail.com>
2021-01-31 16:05:18 +0100

    all projects are now collapses

--- public/index.js
+++ public/index.js
@@ -2,43 +2,35 @@ $.ajax({
     url: '/api/projects'
 }).done(response => {
     response.live.map(page => {
-        appendPage(page, '#live-projects-holder');
+        appendCollapse(page, '#live-projects-holder');
     });
     response.other.map(page => {
-        if (page.type === 'link') appendPage(page, '#other-projects-holder');
-        else appendCollapse(page, '#other-projects-holder');
+        appendCollapse(page, '#other-projects-holder');
     });
 });
-
-function appendPage(data, id) {
-    const pageHolder = $(id);
-
-    pageHolder.append(
-        `<div class="project">` +
-            `<span>` +
-                `<a class="link-holder" target="_blank" href="${data.link}">` +
-                    `<strong> ${data.name}</strong>` +
-                `</a>` +
-                ` : ${data.description}` +
-            `</span>` +
-        `</div>`
-    );
-}
 
 function appendCollapse(data, id) {
     const pageHolder = $(id);
 
+    let linkString = '';
+
+    if (data.links !== undefined) {
+        data.links.map(link => {
+            linkString = linkString + `<a class="link-holder" target="_blank" href="${link.link}"><i class="${link.icon} link-icon"></i></a>`;
+        });
+    }
+
     pageHolder.append(
         `<div class="project">
             <span>
-                <a class="link-holder collapse-trigger" href="#${data.name}" data-toggle="collapse" aria-expanded="false" aria-controls="skills-collapse">
+                <a class="link-holder collapse-trigger" href="#${data.name.replaceAll(' ', '_')}" data-toggle="collapse" aria-expanded="false" aria-controls="skills-collapse">
                     <strong>${data.name}</strong>
                 </a>
                  : ${data.description}
             </span>
         </div>
-        <div class="collapse" id="${data.name}">
-            <div class="collapse-text">${data.text}</div>
+        <div class="collapse" id="${data.name.replaceAll(' ', '_')}">
+            <div class="collapse-text">${data.text}<br>${linkString}</div>
         </div>`
     );
 }
